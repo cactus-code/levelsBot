b676cd646879a582c17ff646e3924191cffd55b7
cactus-code <cactus-code@users.noreply.github.com>
2017-06-22 19:15:41 +1200

    Fixed bugs

--- levelBot.py
+++ levelBot.py
@@ -37,6 +37,7 @@ def get_stars_string(ctx):
         else:
             string_list.append(string)
             string = ""
+    string_list.append(string)
     return string_list
 
 @level_bot.event
