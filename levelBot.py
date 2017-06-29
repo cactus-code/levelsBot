bddec8abd39e859c0d820d8858ff3aea21d0534c
cactus-code <cactus-code@users.noreply.github.com>
2017-06-29 14:01:13 +1200

    Removed try, except

--- levelBot.py
+++ levelBot.py
@@ -31,18 +31,15 @@ def get_stars_string(ctx):
     string_list = []
     string = ""
     for key in player_stars:
-        try:
-            if len(string) < 1900:
-                need_append = True
-                name_object = get_username(ctx,key)
-                string = string + name_object.display_name + " : " + player_stars[key] + "\n"
-            else:
-                need_append = False
-                string = string + name_object.display_name + " : " + player_stars[key] + "\n"
-                string_list.append(string)
-                string = ""
-        except:
-            pass
+        if len(string) < 1900:
+            need_append = True
+            name_object = get_username(ctx,key)
+            string = string + name_object.display_name + " : " + player_stars[key] + "\n"
+        else:
+            need_append = False
+            string = string + name_object.display_name + " : " + player_stars[key] + "\n"
+            string_list.append(string)
+            string = ""
     if need_append:
         string_list.append(string)
     return string_list
